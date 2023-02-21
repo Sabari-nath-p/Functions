ce28b2e3c1d6e99768bf7d8f885d422b9c9a2472
Sabari-nath-p <sabarinath5604@gmail.com>
2023-02-21 22:08:06 +0530

    chat listing function

--- message.dart
+++ message.dart
@@ -2,34 +2,79 @@ import 'package:firebase_database/firebase_database.dart';
 
 class Message {
   String? senderId;
+  String? receiverId;
   String? messageId;
-  List<MessageData> chat = [];
+  bool IsReady = false;
 
-  Message({required this.messageId, required this.senderId});
-  DatabaseReference messageDatabase = FirebaseDatabase.instance.ref("message");
+  Message({
+    required this.senderId,
+    required this.receiverId,
+    this.messageId = "",
+  }) {
+    if (messageId == "") {
+      messageDatabase
+          .child("chat_list")
+          .child(senderId!)
+          .child(receiverId!)
+          .child("message_id")
+          .once()
+          .then((value) {
+        print(value);
+        if (value.snapshot.exists) {
+          print(value.snapshot.exists);
+          messageId = value.snapshot.value.toString();
+        }
+      });
+    }
+  }
+  DatabaseReference messageDatabase = FirebaseDatabase.instance.ref();
 
   sendMessage(String messageString) {
     String status = "waiting";
     DateTime dt = DateTime.now();
-    messageDatabase.child(messageId!).push().set({
+
+    if (messageId == "") {
+      messageId = messageDatabase.child("message").push().key;
+    }
+    messageDatabase.child("message").child(messageId!).push().set({
       "date_time": dt.toString(),
       "message": messageString,
       "sender_id": senderId
     });
+
+    messageDatabase.child("chat_list").child(senderId!).child(receiverId!).set({
+      "last_message": messageString,
+      "date_time": dt.toString(),
+      "message_id": messageId,
+      "last": senderId
+    });
+    messageDatabase
+        .child("chat_list")
+        .child(receiverId!)
+        .child(messageId!)
+        .set({
+      "last_message": messageString,
+      "date_time": dt.toString(),
+      "last": senderId
+    });
   }
 
   startMessageListerner(
-    Function(List chatMessage) onMessage,
+    Function(List<MessageData> chatMessage) onMessage,
   ) {
-    messageDatabase.child(messageId!).onValue.listen((event) {
-      chat.clear();
+    print(messageId);
+    messageDatabase.child("message").child(messageId!).onValue.listen((event) {
+      List<MessageData> chat = [];
+      if (event.snapshot.exists) {
+        IsReady = true;
+      }
       for (var data in event.snapshot.children) {
         MessageData mg = MessageData(
             message: data.child("message").value.toString(),
             dateTime: data.child("date_time").value.toString(),
             messageId: data.key,
             senderId: data.child("sender_id").value.toString());
-
+        print(mg.senderId);
         if (mg.senderId == senderId) mg.from = true;
         chat.add(mg);
       }
---
+++ messageList.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_database/firebase_database.dart';
+
+class MessageList {
+  String? user_id; // user id of current user
+  DatabaseReference db = FirebaseDatabase.instance.ref("chat_list");
+  MessageList({required this.user_id});
+
+  chatLister(Function(List<chatData> chatData) newMessage) {
+    db.child(user_id!).orderByChild("data_time").onValue.listen((event) {
+      List<chatData> chatList = [];
+      for (var chat in event.snapshot.children) {
+        chatData data = chatData(
+            channelID: chat.child("message_id").value.toString(),
+            dateTime: chat.child("date_time").value.toString(),
+            receiverID: chat.key,
+            lastMessage: chat.child("last_message").value.toString());
+        chatList.add(data);
+        //    print(chatList[0].channelID);
+      }
+
+      chatList.sort((a, b) => DateTime.parse(a.dateTime.toString())
+          .compareTo(DateTime.parse(b.dateTime.toString())));
+      //  print(chatList[0].channelID);
+      newMessage(chatList);
+    });
+  }
+}
+
+class chatData {
+  String? lastMessage;
+  String? dateTime;
+  String? channelID;
+  String? receiverID;
+
+  chatData(
+      {required this.channelID,
+      required this.dateTime,
+      required this.receiverID,
+      required this.lastMessage});
+}
